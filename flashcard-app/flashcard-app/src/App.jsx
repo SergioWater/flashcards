acefb65a878c1a8fe0127c2f023f7a6db6669dcd
Serg <104388314+SergioWater@users.noreply.github.com>
2025-02-17 18:16:11 -0800

    app

---
+++ flashcard-app/flashcard-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import FlashcardQuiz from './Flashcard.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <FlashcardQuiz />
+    </div>
+  );
+}
+
+export default App;
--- types.ts
+++
@@ -1,9 +0,0 @@
-export type QuizCard = {
-    id: number
-    question: string
-    options: string[]
-    answer: string
-    explanation: string
-  }
-  
-  
